8506f0b729f201749e7b02fd27df2ddf6f121625
Jintu Nath <jintukumarnathnath@gmail.com>
2023-05-15 20:16:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,render_template
 import pickle
 import requests
-import pandas
+import pandas as pd
 # from patsy import dmatrices
 
 # with open('movies_list.pkl', 'rb') as f:
